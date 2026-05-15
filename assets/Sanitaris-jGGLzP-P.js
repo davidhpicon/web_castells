01c90f925c767e90be0442d90543b0d20bb59167
davidhpicon <davidhpicon@users.noreply.github.com>
2026-05-15 17:00:18 +0000

    deploy: 6306e7a849f52bd934fa27f80852a08ba439dee9

---
+++ assets/Sanitaris-jGGLzP-P.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-D0peT6X_.js";function a(){return s.jsx(s.Fragment,{children:s.jsx("div",{className:"page-hero",children:s.jsxs("div",{className:"container",children:[s.jsxs("h1",{children:["Els nostres ",s.jsx("span",{children:"Sanitaris"})]}),s.jsx("p",{children:"L'equip que vetlla per la salut i la seguretat de tots els castellers."})]})})})}export{a as default};
